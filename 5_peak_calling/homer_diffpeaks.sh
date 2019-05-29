f71852a51e69275499df71fc031077df056c6be2
MessaoudiLab <42880500+MessaoudiLab@users.noreply.github.com>
2019-05-29 11:54:42 -0700

    Update homer_diffpeaks.sh

--- 5_peak_calling/homer_diffpeaks.sh
+++ 5_peak_calling/homer_diffpeaks.sh
@@ -5,11 +5,12 @@ shift
 targettag=$1
 shift
 backtag=$1
-shift 
+shift
 out=$1
 shift
 
 module load samtools
 module load bedtools
 
-/bigdata/messaoudilab/ssure003/Projects/Maob_Mom_CD14_ATAC/paper-jan2019/scripts/Homer/bin/getDifferentialPeaks $peaks $targettag $backtag -P 0.05 > $out.diffpeaks.txt
+/bigdata/messaoudilab/ssure003/Projects/Maob_Mom_CD14_ATAC/paper-jan2019/scripts/Homer/bin/getDifferentialPeaks $peaks $targettag $backtag -P 0.05 > $out.positivefc.diffpeaks.txt
+/bigdata/messaoudilab/ssure003/Projects/Maob_Mom_CD14_ATAC/paper-jan2019/scripts/Homer/bin/getDifferentialPeaks $peaks $backtag $targettag -P 0.05 > $out.negativefc.diffpeaks.txt
